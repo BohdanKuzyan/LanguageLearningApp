c26b66a6fcb14a68284a204b2adc97a6fdec61de
Pioskl <115486249+Pioskl@users.noreply.github.com>
2024-11-20 08:08:03 +0100

    dodanie rejestracji
    Hasło do poprawy

---
+++ LanguageLearningApp/Models/Kody_Rejestracji.cs
@@ -0,0 +1,9 @@
+﻿namespace LanguageLearningApp.Models;
+using System.ComponentModel.DataAnnotations;
+public class Kody_Rejestracji
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string KOD { get; set; }
+}
